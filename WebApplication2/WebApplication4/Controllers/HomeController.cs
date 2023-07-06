a344e83a6ec01618eecbc2552fb631cd92012a40
suika <raffidahmad@gmail.com>
2023-07-06 15:10:19 +0500

    init

---
+++ WebApplication2/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using WebApplication4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        List<person> perinfo;
+
+        public HomeController()
+        {
+            perinfo = new List<person>();
+            perinfo.Add(new person { FirstName = "Muhammad", LastName = "Tayyab", Email="mtayyabm222@gmail.com", MobileNumber="0300-0000000", Gender="Male", Address= "Wapda town,Lahore Pakistan", City="Lahore", pincode=5447, state="Punjab", coun="Pakistan", classXB="Lahore", classXP="80", classXY="2018", classXIIB="Lahore", classXIIP="80", classXIIY="2020", GraduationB="Lahore", GraduationP="3.3CGPA", GraduationY="2024", MastersB="Lahore", MastersP="3CGPA", MastersY="2027" });
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View(perinfo);
+        }
+
+        [HttpPost]
+        public ActionResult Index(person postinfo)
+        {
+            perinfo.Add(postinfo);
+            return View("List",perinfo);
+        }
+
+    }
+}
---
+++ WebApplication2/WebApplication4/Models/Class.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class person
+    {
+        [RegularExpression("^[a-zA-Z]{1,30}$", ErrorMessage = "Maximum 30 characters and alphabets only")]
+        [DisplayName("First name")]
+        public string? FirstName { get; set; }
+
+        [RegularExpression("^[a-zA-Z]{1,30}$", ErrorMessage = "Maximum 30 characters and alphabets only")]
+        [DisplayName("Last name")]
+        public string? LastName { get; set; }
+
+        public string? Date { get; set; }
+        public string? month { get; set; }
+        public string? year { get; set; }
+
+        [DisplayName("Email ID")]
+        public string? Email { get; set; }
+
+        [RegularExpression("^[0-9]{11}$", ErrorMessage = "11 Digit Number")]
+        [Required]
+        [DisplayName("Mobile Number")]
+        public string? MobileNumber { get; set; }
+
+        [DisplayName("Gender")]
+        public string? Gender { get; set; }
+
+        [DisplayName("Address")]
+        public string? Address { get; set; }
+
+        [RegularExpression("^[a-zA-Z]{1,30}$", ErrorMessage = "Maximum 30 characters and alphabets only")]
+        [DisplayName("City")]
+        public string? City { get; set; }
+
+        [RegularExpression("^[0-9]{6}$", ErrorMessage = "6 Digit Number")]
+        [DisplayName("Pin Code")]
+        public int? pincode { get; set; }
+
+        [RegularExpression("^[a-zA-Z]{1,30}$", ErrorMessage = "Maximum 30 characters and alphabets only")]
+        [DisplayName("State")]
+        public string? state { get; set; }
+
+        [DisplayName("Country")]
+        public string? coun { get; set; }
+
+        [DisplayName("Hobbies")]
+		public List<string?> Hobb { get; set; }
+
+        [Required(ErrorMessage ="Qualification is required")]
+        //[RegularExpression("^[A-Za-z]{1,10}$", ErrorMessage = "Maximum 10 characters")]
+        public string? classXB { get; set; }
+
+        [RegularExpression("^[0-9]{2,50}$", ErrorMessage = "Upto 2 decimal Characters")]
+        [Required]
+        public string? classXP { get; set; }
+        public string? classXY { get; set; }
+
+        //[RegularExpression("^[A-Za-z]{1,10}$", ErrorMessage = "Maximum 10 characters")]
+        [Required]
+        public string? classXIIB { get; set; }
+
+        [RegularExpression("^[0-9]{2,50}$", ErrorMessage = "Upto 2 decimal Characters")]
+        [Required]
+        public string? classXIIP { get; set; }
+        public string? classXIIY { get; set; }
+
+        [RegularExpression("^[A-Za-z]{1,10}$", ErrorMessage = "Maximum 10 characters")]
+        [Required]
+        public string? GraduationB { get; set; }
+
+        [RegularExpression("^[0-9]{2,50}$", ErrorMessage = "Upto 2 decimal Characters")]
+        [Required]
+        public string? GraduationP { get; set; }
+        public string? GraduationY { get; set; }
+
+        //[RegularExpression("^[A-Za-z]{1,10}$", ErrorMessage = "Maximum 10 characters")]
+        [Required]
+        public string? MastersB { get; set; }
+
+        [RegularExpression("^[0-9]{2,50}$", ErrorMessage = "Upto 2 decimal Characters")]
+        [Required]
+        public string? MastersP { get; set; }
+        public string? MastersY { get; set; }
+
+
+        [DisplayName("Courses Applied for")]
+        public string? courses { get; set; }
+    }
+}
